e198d73fefa509f2d5c1e1b57d6b7c20bf1b1bd4
huttongrabiel <huttonthomas@icloud.com>
2022-05-02 17:25:38 -0700

    Array: Create Array header file + implement some functions
    Implement:
        - size()
        - empty()
        - fill(const T&)
        - data()
        - operator[]
    
    Create the array of type T elements. Have const and non-const versions
    for most functions so that we can call the array as constexpr.

---
+++ DTSLib/Array.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022, Hutton Grabiel
+ */
+
+#pragma once
+
+#include <iostream>
+
+namespace DTS {
+
+template<typename T, size_t Size>
+struct Array {
+    using ValueType = T;
+    using Reference = T&;
+    using ConstReference = T const&;
+
+    T arr[Size];
+
+    [[nodiscard]] constexpr size_t size() const { return Size; }
+
+    [[nodiscard]] constexpr bool empty() const { return size() == 0; }
+
+    constexpr void fill(const T& u) {
+        for (size_t i = 0; i < Size; i++) {
+            arr[i] = u;
+        }
+    }
+
+    constexpr T* data()
+    {
+        return arr;
+    }
+
+    constexpr T const* data() const
+    {
+        return arr;
+    }
+
+    constexpr Reference operator[](size_t index)
+    {
+        for (size_t i = 0; i < Size; i++) {
+            if (i == index)
+                return arr[i];
+        }
+        return arr[0];
+    }
+
+    constexpr ConstReference operator[](size_t index) const
+    {
+        for (size_t i = 0; i < Size; i++) {
+            if (i == index)
+                return arr[i];
+        }
+        return arr[0];
+    }
+};
+
+}
